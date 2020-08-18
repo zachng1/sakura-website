746a3a0422fc4a34fd0456068bd99ab4d6b1e421
Zac Hing <zachng1@gmail.com>
2020-08-18 19:45:14 +1200

    update mobile format

--- scripts.js
+++ scripts.js
@@ -1,22 +1,36 @@
-const linkImgHoverToLink = (id) => {
+const linkImgHoverToLink = (id, defaultColor) => {
     $(id).hover(() => {
         $(id + "Link").css("color", "olive");
     }, () => {
-        $(id + "Link").css("color", "black");
+        $(id + "Link").css("color", defaultColor);
     });
     //this second one needs to be here because .css will override
     //any pseudo-elements (i.e. a:hover)
     $(id + "Link").hover(() => {
         $(id + "Link").css("color", "olive");
     }, () => {
-        $(id + "Link").css("color", "black");
+        $(id + "Link").css("color", defaultColor);
+    });
+    $(id + "Link").css("color", defaultColor);
+}
+
+const colorOnResize = () => {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    if (vw < 640) {
+        iterateChildrenHover("white");
+    }
+    else {
+        iterateChildrenHover("black");
+    }
+}
+
+const iterateChildrenHover = (color) => {
+    $("#main").children().each((index, child) => {
+        linkImgHoverToLink("#" + child.attributes["id"].value, color);
     });
 }
 
 $(document).ready(() => {
-    console.log($("#main"));
-    $("#main").children().each((index, child) => {
-        console.log(child);
-        linkImgHoverToLink("#" + child.attributes["id"].value);
-    });
+    iterateChildrenHover("black");
+    window.addEventListener("resize", colorOnResize);
 });
